5f1b290992ca72bc938691ec0e5974797c79dd12
Ana García Remesal <anagr2002cursos@gmail.com>
2024-10-11 10:13:06 +0200

    1º Commit

---
+++ src/test/CalculadoraTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	void testSuma() {
+		asserEquals(5, Calculadora.suma(2,3));
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
